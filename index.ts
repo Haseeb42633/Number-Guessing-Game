0242b2a0c54feb30acd785c2843c7e70ca160f47
Haseeb42633 <haaseebiqbal@gmail.com>
2024-06-08 03:25:53 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const maxGuesses = 4;
+const randomNumber: number = Math.floor(Math.random() * 10);
+
+type numType = {
+    "userGuess": number
+}
+
+async function guessNumber() {
+    let guessesLeft = maxGuesses; 
+    let correctGuess = false;
+
+    while (guessesLeft > 0 && !correctGuess) {
+        const answers: numType = await inquirer.prompt([
+            {
+                type: "number",
+                name: "userGuess",
+                message: `Guess a number between 0 to 10 and write it (You have ${guessesLeft} guesses left):`,
+            },
+        ]);
+
+        const { userGuess } = answers;
+        console.log(`Guessed No: ${userGuess}, Correct answer is: ${randomNumber}`);
+
+        if (userGuess === randomNumber) {
+            console.log("Correct Answer! Congratulations, you won!");
+            correctGuess = true;
+        }
+        else {
+            console.log("Wrong Answer!");
+            guessesLeft--;
+            if (guessesLeft > 0) {
+                console.log(`You have ${guessesLeft} guesses left.`);
+            }
+        }
+    }
+
+    if (!correctGuess) {
+        console.log(`Out of guesses. The correct number was ${randomNumber}. Better luck next time!`);
+    }
+}
+
+guessNumber();
